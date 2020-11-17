5fdf625379ba7349a48c5ee5509999222748ebc4
TyrionJYQ <1003518719@qq.com>
2020-11-17 22:08:42 +0800

    update index

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,8 +1,14 @@
 <template>
 	<view class="content bg-color">
-		<view class="text-area">
-			首页
+		<view>
+			<u-tabs-swiper ref="uTabs" :list="list" :current="current" @change="tabsChange" :is-scroll="false" swiperWidth="750"
+			 class="tabs" active-color="#19be6b"></u-tabs-swiper>
 		</view>
+		<swiper :current="swiperCurrent" @transition="transition" @animationfinish="animationfinish" class="swiper-wrapper">
+			<swiper-item class="swiper-item" v-for="(item, index) in list" :key="index">
+				<view class="swiper-item uni-bg-red">{{item.name}}</view>
+			</swiper-item>
+		</swiper>
 	</view>
 </template>
 
@@ -43,37 +49,83 @@
 			}
 		},
 
+		data() {
+			return {
+				list: [{
+					name: '心情'
+				}, {
+					name: '待办'
+				}, {
+					name: '清单'
+				}],
+				// 因为内部的滑动机制限制，请将tabs组件和swiper组件的current用不同变量赋值
+				current: 0, // tabs组件的current值，表示当前活动的tab选项
+				swiperCurrent: 0, // swiper组件的current值，表示当前那个swiper-item是活动的
+			};
+		},
+
+
 		methods: {
-			...mapActions(['getUserOpenId', 'setYearFeels', 'getTodoList', 'setListData'])
+			...mapActions(['getUserOpenId', 'setYearFeels', 'getTodoList', 'setListData']),
+			tabsChange(index) {
+				this.current = index
+				this.swiperCurrent = index;
+			},
+			// swiper-item左右移动，通知tabs的滑块跟随移动
+			transition(e) {
+				let dx = e.detail.dx;
+				this.$refs.uTabs.setDx(dx);
+			},
+			// 由于swiper的内部机制问题，快速切换swiper不会触发dx的连续变化，需要在结束时重置状态
+			// swiper滑动结束，分别设置tabs和swiper的状态
+			animationfinish(e) {
+				let current = e.detail.current;
+				this.$refs.uTabs.setFinishCurrent(current);
+				this.swiperCurrent = current;
+				this.current = current;
+			},
+			// scroll-view到底部加载更多
+			onreachBottom() {
+
+			}
 		}
 	}
 </script>
 
-<style>
+<style scoped less>
 	page {
 		height: 100%;
 	}
+
 	.content {
 		display: flex;
 		flex-direction: column;
-		align-items: center;
-		justify-content: center;
+
 		height: 100vh;
 		padding: 15rpx;
 		box-sizing: border-box;
-		
+
 	}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
+	.swiper-wrapper {
+		flex: 1;
+
+		background-color: #FFFFFF;
+		margin-top: 15rpx;
+		border-radius: 5rpx;
+
 	}
 
-	
+	/deep/ .swiper-item {
+		display: flex;
+		/* align-items: center; */
+		justify-content: center;
+	}
+
+	/deep/ .u-tabs {
+		border-radius: 5rpx;
+	}
+
 
 	.title {
 		font-size: 36rpx;
